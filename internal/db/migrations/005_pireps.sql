3226d15c44fae35790ab15ed516a7825966b5fad
sanketpandia <sanketpandia@gmail.com>
2025-08-08 20:30:06 +0530

    query update

--- internal/db/migrations/005_pireps.sql
+++ internal/db/migrations/005_pireps.sql
@@ -1,13 +1,5 @@
 
 ALTER TABLE va_user_roles 
-ADD COLUMN IF NOT EXISTS callsign VARCHAR(40)
+ADD COLUMN IF NOT EXISTS callsign VARCHAR(40);
 
 ALTER TYPE va_role RENAME VALUE 'airline_manager' TO 'staff';
-
--- Drop the old unique constraint
-ALTER TABLE va_user_roles
-DROP CONSTRAINT va_user_roles_user_id_va_id_role_key;
-
--- Add the new unique constraint
-ALTER TABLE va_user_roles
-ADD CONSTRAINT va_user_roles_user_id_va_id_key UNIQUE (user_id, va_id);
